20f60a06a267bc94b104076d9fa510e9669eabf3
hochshi <hochshi@users.noreply.github.com>
2023-02-20 22:25:20 +0200

    Added fgt direct with weights and formatted main.cpp according to clang-format.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/eigen.h>
 #include <fgt.hpp>
-
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -9,7 +8,7 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(pyfgt, m) {
-    m.doc() = R"pbdoc(
+  m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
 
@@ -22,16 +21,28 @@ PYBIND11_MODULE(pyfgt, m) {
            subtract
     )pbdoc";
 
-    m.def("direct", [](const Eigen::MatrixXd& x, const Eigen::MatrixXd& y, double bandwidth) {
-            return fgt::direct(x, y, bandwidth);
-    });
-    m.def("direct3", [](const Eigen::Matrix<double, Eigen::Dynamic, 3>& x, const Eigen::Matrix<double, Eigen::Dynamic, 3>& y, double bandwidth) {
-            return fgt::direct(x, y, bandwidth);
-    });
+  m.def("direct",
+        [](const Eigen::MatrixXd &x, const Eigen::MatrixXd &y,
+           double bandwidth) { return fgt::direct(x, y, bandwidth); });
 
+  m.def("wdirect", [](const Eigen::MatrixXd &x, const Eigen::MatrixXd &y,
+                      double bandwidth, const Eigen::VectorXd weights) {
+    return fgt::direct(x, y, bandwidth, weights);
+  });
+
+  m.def("direct3",
+        [](const Eigen::Matrix<double, Eigen::Dynamic, 3> &x,
+           const Eigen::Matrix<double, Eigen::Dynamic, 3> &y,
+           double bandwidth) { return fgt::direct(x, y, bandwidth); });
+
+  m.def("wdirect3", [](const Eigen::Matrix<double, Eigen::Dynamic, 3> &x,
+                       const Eigen::Matrix<double, Eigen::Dynamic, 3> &y,
+                       double bandwidth, const Eigen::VectorXd weights) {
+    return fgt::direct(x, y, bandwidth, weights);
+  });
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
